3e0758f6ab7a3ab224f8fe88dd9cb01589c1433b
zengle <1454842194@qq.com>
2021-07-12 08:43:43 +0800

    version1

---
+++ src/main/java/com/zl/Test.java
@@ -0,0 +1,15 @@
+package com.zl;
+
+/**
+ * @author zengle
+ * @create 2021-07-11 17:20
+ */
+public class Test {
+  public static void main(String[] args) {
+
+    for (int i = 0; i < 10; i++) {
+      //
+      System.out.println("commit: "+i);
+    }
+  }
+}
